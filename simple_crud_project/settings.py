02902b1fcc6bb47ddddf16b9b042e7fba268f64d
ReddyAdhi <reddyadhi94@gmail.com>
2022-06-20 19:19:58 +0700

    Add crud_app and migrations

--- simple_crud_project/settings.py
+++ simple_crud_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local app
+    'crud_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'simple_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'simple_crud_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simple_crud',
+        'USER': 'postgres',
+        'PASSWORD': 'user1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
